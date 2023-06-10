ed9c8eda1c1d9d014172c2550f37dc03698e982f
sallyvann.ange <ange.sallyvann@ext-bpifrance.fr>
2023-06-10 02:12:29 +0200

    Created triggers and routines

--- create_tables.sql
+++ create_tables.sql
@@ -101,7 +101,64 @@ FOR EACH ROW
         WHERE id = NEW.size_id
     );
 
+-- Créé un trigger qui va changer la colonne orders.updated_at lorsque le statut d'une commande est modifiée
+CREATE TRIGGER update_order_updated_at
+BEFORE UPDATE ON orders
+FOR EACH ROW
+    SET NEW.updated_at = CURRENT_TIMESTAMP;
+
 -- Routines
+-- Obtenir les ingrédients composant une pizza
+DELIMITER $$
+CREATE PROCEDURE get_pizza_ingredients(IN pizza_id INT)
+BEGIN
+    SELECT i.id, i.name
+    FROM ingredients i
+    INNER JOIN pizza_ingredients pi ON pi.ingredient_id = i.id
+    WHERE pi.pizza_id = pizza_id;
+END $$
+DELIMITER ;
+-- Obtenir les pizzas triés par popularité en fonction du nombre d'occurence dans orders
+DELIMITER $$
+CREATE PROCEDURE get_pizzas_by_popularity()
+BEGIN
+    SELECT p.id , COUNT(*) AS popularite
+    FROM pizzas p
+    INNER JOIN orders o ON o.pizza_id = p.id
+    WHERE o.status IN ('En cours', 'Validé')
+    GROUP BY p.id
+    ORDER BY popularite DESC;
+END $$
+DELIMITER ;
+
+-- Obtenir les ingrédients triés par popularité en fonction du nombre d'occurence parmi les pizzas commandées dans orders
+DELIMITER $$
+CREATE PROCEDURE get_ingredients_by_popularity()
+BEGIN
+    SELECT i.id, i.name, COUNT(*) AS popularite
+    FROM ingredients i
+    INNER JOIN pizza_ingredients pi ON pi.ingredient_id = i.id
+    INNER JOIN orders o ON o.pizza_id = pi.pizza_id
+    WHERE o.status IN ('En cours', 'Validé')
+    GROUP BY i.id
+    ORDER BY popularite DESC;
+END $$
+DELIMITER ;
+
+-- Obtenir les drivers triés par nombre de retard de livraison : on considère une livraison comme retardée si il y a plus de 30 minutes écoulées entre orders.created_at et orders.updated_at
+DELIMITER $$
+CREATE PROCEDURE get_drivers_by_delivery_delay()
+BEGIN
+    SELECT d.id, COUNT(*) AS delivery_delays
+    FROM drivers d
+    JOIN orders o ON o.driver_id = d.id
+    WHERE TIMEDIFF(o.updated_at, o.created_at) > '00:30:00' AND o.status = 'Livré'
+    GROUP BY d.id
+    ORDER BY delivery_delays DESC;
+END $$
+DELIMITER ;
+
+
 -- Calculer la moyenne des commandes
 DELIMITER $$
 CREATE PROCEDURE get_orders_average() 
@@ -110,3 +167,84 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- Obtenir clients par ordre descendants de nombre de commandes
+DELIMITER $$
+CREATE PROCEDURE get_clients_by_total_orders() 
+BEGIN
+    SELECT client_id, COUNT(*) AS commandes_effectuees
+    FROM orders
+    WHERE status IN ('En cours', 'Validé')
+    GROUP BY client_id
+    ORDER BY commandes_effectuees DESC;
+END $$
+DELIMITER ;
+
+-- Obtenir le nombre de commande par client
+DELIMITER $$
+CREATE PROCEDURE get_orders_count_by_client()
+BEGIN
+    SELECT client_id, COUNT(*) AS commandes_effectuees
+    FROM orders
+    WHERE status IN ('En cours', 'Validé')
+    GROUP BY client_id;
+END $$
+DELIMITER ;
+
+-- Obtenir le nombre de commande du client défini par client_id
+DELIMITER $$
+CREATE PROCEDURE get_orders_count_by_client_id(IN id INT)
+BEGIN
+    SELECT COUNT(*) AS commandes_effectuees
+    FROM orders
+    WHERE status IN ('En cours', 'Validé')
+    AND client_id = id;
+END $$
+DELIMITER ;
+
+-- Calculer le nombre de commandes effectuées en moyenne par les clients
+DELIMITER $$
+CREATE PROCEDURE get_average_orders_by_client()
+BEGIN
+    SELECT AVG(commandes_effectuees) AS commandes_moyennes
+    FROM (
+        SELECT client_id, COUNT(*) AS commandes_effectuees
+        FROM orders
+        WHERE status IN ('En cours', 'Validé')
+        GROUP BY client_id
+    ) AS commandes_par_client;
+END $$
+DELIMITER ;
+
+-- Récupérer les clients ayant effectué plus de commandes que la moyenne
+DELIMITER $$
+CREATE PROCEDURE get_clients_with_more_orders_than_average()
+BEGIN
+    SELECT client_id, COUNT(*) AS commandes_effectuees
+    FROM orders
+    WHERE status IN ('En cours', 'Validé')
+    GROUP BY client_id
+    HAVING commandes_effectuees > (
+        SELECT AVG(commandes_effectuees) AS commandes_moyennes
+        FROM (
+            SELECT client_id, COUNT(*) AS commandes_effectuees
+            FROM orders
+            WHERE status IN ('En cours', 'Validé')
+            GROUP BY client_id
+        ) AS commandes_par_client
+    );
+END $$
+DELIMITER ;
+
+-- Récupérer les véhicules n'ayant jamais servi
+DELIMITER $$
+CREATE PROCEDURE get_unused_vehicles()
+BEGIN
+    SELECT *
+    FROM vehicles
+    WHERE id NOT IN (
+        SELECT vehicle_id
+        FROM orders
+        WHERE vehicle_id IS NOT NULL
+    );
+END $$
+DELIMITER ;
--- drop_tables.sql
+++ drop_tables.sql
@@ -11,6 +11,15 @@ DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS vehicles;
 DROP TABLE IF EXISTS sizes;
 
+DROP TRIGGER IF EXISTS update_order_amount;
 DROP PROCEDURE IF EXISTS get_orders_average;
+DROP PROCEDURE IF EXISTS get_clients_by_total_orders;
+DROP PROCEDURE IF EXISTS get_orders_count_by_client;
+DROP PROCEDURE IF EXISTS get_average_orders_by_client;
+DROP PROCEDURE IF EXISTS get_unused_vehicles;
+DROP PROCEDURE IF EXISTS get_clients_with_more_orders_than_average;
+
+
+
 
 SET FOREIGN_KEY_CHECKS = 1;
